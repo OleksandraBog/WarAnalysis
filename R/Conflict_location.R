cd96429c24aa3cb8745a72af8d1c60420ed33d09
OleksandraBog”git config --global user.email “oleksandrabogd@gmail.com”git config --global user.name OleksandraBog” <oleksandrabogd@gmail.com>
2024-07-18 22:20:40 +0200

    Update

---
+++ R/Conflict_location.R
@@ -0,0 +1,14 @@
+#' Conflict Locations Data
+#'
+#' A dataset containing the locations of conflict incidents with their respective latitude and longitude.
+#'
+#' @format A data frame with 9 rows and 3 variables:
+#' \describe{
+#'   \item{location}{A character vector representing the name of the location.}
+#'   \item{latitude}{A numeric vector representing the latitude of the location.}
+#'   \item{longitude}{A numeric vector representing the longitude of the location.}
+#' }
+#' @examples print(conflict_locations)
+#'   plot_conflict_map(conflict_locations)
+
+"conflict_locations"
